3f36a547000faa363d01f298e00d7d3a9b5c9f92
Moran-38B <chococa.minecraft@gmail.com>
2026-09-26 00:00:50 +0800

    Update RoleItem.cs

--- Werewolf/Models/RoleItem.cs
+++ Werewolf/Models/RoleItem.cs
@@ -1,12 +1,23 @@
 namespace Werewolf.Models
 {
+    // 定義所有角色的強型別 Enum
+    public enum Role
+    {
+        Unknown,
+        Villager, OldHooligan,
+        Werewolf, WolfKing, WhiteWolfKing, GhostRider, Gargoyle, WolfBeauty, BloodMoonApostle, HiddenWolf,
+        Seer, Witch, Hunter, Guard, Idiot, Bomber, Knight, Silencer, BearTamer, GraveKeeper, DemonHunter, Dreamcatcher, Magician, Crow,
+        Cupid, WildChild, Thief
+    }
+
     public class RoleItem
     {
+        public Role Role { get; set; } = Role.Unknown; // 加回強型別
         public string Name { get; set; } = "";
-        public string Faction { get; set; } = ""; // 好人、狼人、第三方
+        public string Faction { get; set; } = "";
         public int Count { get; set; } = 0;
         
-        // 新增：角色詳細資訊模板
+        // 百科擴充屬性
         public string Description { get; set; } = "";
         public string Advantage { get; set; } = "";
         public string Disadvantage { get; set; } = "";
