e2a787fc22dda7b685c83a3fbbcb48842393bbac
Moran-38B <chococa.minecraft@gmail.com>
2026-09-25 20:54:22 +0800

    Update Program.cs

--- Werewolf/Program.cs
+++ Werewolf/Program.cs
@@ -10,6 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
-builder.Services.AddSingleton<GamrManager>();
+builder.Services.AddSingleton<GameManager>();
 
 await builder.Build().RunAsync();
