d1c0fda6f99ca6d115d41cf8f49585ab9b6c45c9
Moran-38B <chococa.minecraft@gmail.com>
2026-09-25 21:50:29 +0800

    Update RoleSetup.cs

--- Werewolf/Models/RoleSetup.cs
+++ Werewolf/Models/RoleSetup.cs
@@ -2,32 +2,9 @@ using System.Collections.Generic;
 
 namespace Werewolf.Models
 {
-    // 遊戲規則設定 (獨立存在)
-    public class GameRule
-    {
-        public bool IsKillSide { get; set; } = true; // true = 屠邊, false = 屠城
-        public bool WitchCanSaveSelf { get; set; } = false; // 女巫首夜能否自救
-        public bool DoubleDeadValid { get; set; } = false; // 同守同救是否存活
-    }
-
-    // 單一角色項目 (剛剛不小心遺失的類別)
-    public class RoleItem
-	{
-    	public string Name { get; set; } = "";
-    	public string Faction { get; set; } = "";
-    	public int Count { get; set; } = 0;
-    
-    	// 百科擴充屬性
-    	public string Description { get; set; } = "";
-    	public string Advantage { get; set; } = "";
-		public string Disadvantage { get; set; } = "";
-	}
-
-
-    // 角色配置 (不再包含 Rule)
     public class RoleSetup
     {
-        public string SetupName { get; set; } = "自訂配置";
+        public string SetupName { get; set; } = "";
         public List<RoleItem> Roles { get; set; } = new();
     }
 }
