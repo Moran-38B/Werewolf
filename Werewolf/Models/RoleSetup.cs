db6871428601fe6000de82eba5142e665d7e5131
Moran-38B <chococa.minecraft@gmail.com>
2026-09-25 18:53:06 +0800

    Update RoleSetup.cs

--- Werewolf/Models/RoleSetup.cs
+++ Werewolf/Models/RoleSetup.cs
@@ -5,7 +5,7 @@ namespace Werewolf.Models
     // 遊戲規則設定
     public class GameRule
     {
-        public bool IsKillSide { get; set; } = true; // true = 屠邊, false = 屠城
+    	public bool IsKillSide { get; set; } = true; // true = 屠邊, false = 屠城
         public bool WitchCanSaveSelf { get; set; } = false; // 女巫首夜能否自救
         public bool DoubleDeadValid { get; set; } = false; // 同守同救是否存活
     }
@@ -15,6 +15,5 @@ namespace Werewolf.Models
     {
         public string SetupName { get; set; } = "自訂配置";
         public List<RoleItem> Roles { get; set; } = new();
-        public GameRule Rule { get; set; } = new(); // 綁定規則
     }
 }
