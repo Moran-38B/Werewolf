1caa0c305365b92f0bf7fbb266448135e52ce74b
Moran-38B <chococa.minecraft@gmail.com>
2026-09-25 20:56:38 +0800

    Update Program.cs

--- Werewolf/Program.cs
+++ Werewolf/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Werewolf;
 using MudBlazor.Services;
+using Werewolf.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
