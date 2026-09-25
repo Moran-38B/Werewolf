35d554b02f972e076453a999fe502266b36307aa
Moran-38B <chococa.minecraft@gmail.com>
2026-09-25 21:04:36 +0800

    Update Player.cs

--- Werewolf/Models/Player.cs
+++ Werewolf/Models/Player.cs
@@ -1,16 +1,21 @@
-﻿namespace Werewolf.Models
+namespace Werewolf.Models
 {
     public class Player
     {
-        //座位編號
+        // 座位編號
         public int SeatNumber { get; set; }
-        //真實身分
-        public Role Role { get; set; } = Role.Unknown;
-        //是否存活
+        
+        // 為了與首頁動態配置連動，改為儲存角色名稱與陣營
+        public string RoleName { get; set; } = "未知身分";
+        public string Faction { get; set; } = "未知";
+        
+        // 是否存活
         public bool IsAlive { get; set; } = true;
-        //是否有警徽
-        public bool HasBadge { get; set; }=false;
-        //死亡原因
+        
+        // 是否有警徽
+        public bool HasBadge { get; set; } = false;
+        
+        // 死亡原因
         public string DeadReason { get; set; } = "";
     }
 }
